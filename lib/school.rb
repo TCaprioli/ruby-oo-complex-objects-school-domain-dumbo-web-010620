b45203078090c71f01d548a433007d699dfb4196
Tyler Caprioli <capriolityler@gmail.com>
2020-01-08 15:29:10 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,8 +8,8 @@ attr_accessor :roster
   
   def add_student(name,grade)
       
-       @roster[grade]||= name
-       @roster[grade] << name
+       @roster[grade]||= [name]
+       
       #if @roster[grade] == nil
        # @roster[grade] = []
        # @roster[grade] << name
