76864987c6c6a451b46152021859f9382f61eec0
Tyler Caprioli <capriolityler@gmail.com>
2020-01-08 13:40:59 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+require "pry"
+class School
+  
+  @@roster = {}
+  
+  
+  def initialize(school_name)
+    @@roster
+  end 
+  def roster 
+   @@roster
+  end
+  def roster=(roster)
+   @@roster = roster
+  end 
+  def add_student(name,grade)
+    
+ end
+ 
+
+binding.pry 
+0 
